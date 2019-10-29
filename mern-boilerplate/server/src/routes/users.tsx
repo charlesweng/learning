6e383ee2a7f2e77413098241cd72076224265319
Charles Weng <cweng@cogolabs.com>
2019-10-29 09:30:49 -0400

    restructure learning folder

--- mern-boilerplate/server/src/routes/users.tsx
+++
@@ -1,9 +0,0 @@
-import * as express from "express";
-const router = express.Router();
-
-/* GET users listing. */
-router.get("/", (req: express.Request, res: express.Response, next: any) => {
-  res.send("respond with a resource");
-});
-
-export default router;
--- mern-boilerplate/server/startup.sh
+++
@@ -1,4 +0,0 @@
-npm run build
-docker build . -t companion
-docker rm companion
-docker run -it --name companion -p 3000:3000 companion:latest
